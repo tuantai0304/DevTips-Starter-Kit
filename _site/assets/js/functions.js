b749b3584d2585e7ec1573faa1bc3c161537adb3
tuantai0304 <tuantai0304@gmail.com>
2015-07-08 20:26:58 +1000

    SmoothScroll function

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,5 +1,30 @@
 $( document ).ready(function() {
 
-  //
-
+	
+  smoothScroll(1000);
+	
 });
+
+function alertTest(str){
+	alert(str);
+}
+
+function smoothScroll(duration){
+	
+	$('a[href^="#"]').on('click', function(e){
+		
+		var target = $( $(this).attr('href') );
+		
+		if (target.length)
+		{
+			e.preventDefault();
+			$('html, body').animate({
+					scrollTop: target.offset().top
+			}, duration);
+				// alertTest(target);
+		}
+		
+		
+	});
+	
+}
